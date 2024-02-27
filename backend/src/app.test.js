e93eb64a366906cded17f3818c2771f063f8cae8
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2024-02-26 19:11:01 -0500

    Update app.test.js

--- backend/src/app.test.js
+++ backend/src/app.test.js
@@ -1,34 +1,40 @@
-const request = require('supertest');
-const app = require('./server.js'); // Asegúrate de que la ruta a tu archivo app sea correcta
+const request = require("supertest");
+const app = require("./server.js"); // Asegúrate de que la ruta a tu archivo app sea correcta
 
-describe('API Routes Test', () => {
-
-  // Prueba para la ruta /health
-  test('GET /health - debe responder con 200 OK', async () => {
-    const response = await request(app).get('/health');
+describe("API Routes Test", () => {
+  test("GET /health - debe responder con 200 OK", async () => {
+    const response = await request(app).get("/health");
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('OK');
+    expect(response.text).toBe("OK");
   });
 
-  // Prueba para la ruta GET /
-  test('GET / - debe responder con mensaje de bienvenida', async () => {
-    const response = await request(app).get('/');
+  test("GET / - debe responder con mensaje de bienvenida", async () => {
+    const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
-    expect(response.text).toContain('¡Bienvenido a la aplicación de gestión de créditos!');
+    expect(response.text).toContain(
+      "¡Bienvenido a la aplicación de gestión de créditos!"
+    );
   });
-
-  
-
 });
 
-describe('GET /apiv1/agencias', () => {
-    test('debe responder con un JSON vacío o con datos', async () => {
-      const response = await request(app).get('/apiv1/agencias');
-      expect(response.statusCode).toBe(200);
-      expect(Array.isArray(response.body)).toBe(true);
-      if (response.body.length > 0) {
-        expect(response.body[0]).toHaveProperty('algunaPropiedad');
-      }
-    });
+describe("GET /apiv1/agencias", () => {
+  test("debe responder con un JSON vacío o con datos", async () => {
+    const response = await request(app).get("/apiv1/agencias");
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    if (response.body.length > 0) {
+      expect(response.body[0]).toHaveProperty("algunaPropiedad");
+    }
   });
+});
 
+describe("GET /apiv1/alquiler", () => {
+  test("debe responder con un JSON vacío o con datos", async () => {
+    const response = await request(app).get("/apiv1/alquiler");
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    if (response.body.length > 0) {
+      expect(response.body[0]).toHaveProperty("algunaPropiedad");
+    }
+  });
+});
