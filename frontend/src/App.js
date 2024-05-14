6436df98bc7cee79ae4321584edd355d2bd91443
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2024-05-14 12:37:13 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
+<<<<<<< HEAD
 import Layout from "./components/Layout/Layout";
 import React from "react";
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> parent of fa0e5ec (Merge branch 'main' of https://github.com/JoelArg02/Alquiler-de-Vehiculos-con-replicacion)
 
 function App() {
-  return <Layout />;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
